95744b4e6b0b54c937d20c94fbbab24faf695512
Adil Mujeeb <mujeeb.adil@gmail.com>
2024-04-18 14:53:05 +0530

    Updated the program

--- python/brute_force_longest_cons_seq.py
+++ python/brute_force_longest_cons_seq.py
@@ -4,6 +4,7 @@
  * The consecutive numbers can be in any order.
  * Input: X[] = [40, 4, 50, 1, 2, 13, 9, 3, 29], Output: 4
  * Explanation: The longest consecutive sequence is 1,2,3 and 4.
+ * UPDATE: change the ouput instead of returning the length, also return the list.
 '''
 def linearsearch(x, item):
 	for i in range(len(x)):
@@ -14,22 +15,29 @@ def linearsearch(x, item):
 
 def find_long_seq(x):
 	longest_seq = 0
-	
+	longest_seq_list = []	
+	longest_seq_list_tmp = []
 	for i in range(len(x)):
 		#print("{0} - {1}".format(i, x[i]))
 		cur_item = x[i]
+		longest_seq_list_tmp = []
+		longest_seq_list_tmp.append(cur_item)
 		cur_len = 1
 		while(linearsearch(x, cur_item+1)):
+			longest_seq_list_tmp.append(cur_item+1)
 			cur_item += 1
 			cur_len += 1
 		if cur_len > longest_seq:
 			longest_seq = cur_len
-	return longest_seq
+			longest_seq_list = longest_seq_list_tmp
+	#return longest_seq
+	return longest_seq_list
 
 def main():
 	x = [40, 4, 50, 1, 2, 13, 9, 3, 29]
-	print(x)
-	print(find_long_seq(x))
+	print("Original list =", x)
+	res = find_long_seq(x)
+	print("Longest sequence list: {0}, length: {1}".format(res, len(res)))
 	
 
 if __name__ == '__main__':
